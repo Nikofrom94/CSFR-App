bc09621aa73566afcd227187e3e7c909b091ad92
niko <nikofrom94@gmail.com>
2025-10-19 17:30:27 +0200

    seed with ability categories

--- my-electron-app/seeders/20251008193147-abilitycategory-seed.js
+++ my-electron-app/seeders/20251008193147-abilitycategory-seed.js
@@ -11,11 +11,16 @@ module.exports = {
     const json_data = get_data('/home/niko/Documents/jdr/CypherSystem/Cypher-SRD-FR/CSCG/csrd-20241001.json')
     //console.log(json_data);
     const ab_categ_list = json_data.ab_categ_list;
+    for(let i=0;i<ab_categ_list.length;i++){
+      const now = new Date();
+      ab_categ_list[i]['createdAt'] = now;
+      ab_categ_list[i]['updatedAt'] = now;
+    }
     //const cscg_json = require( __dirname + "/../../../../Documents/jdr/CypherSystem/Cypher-SRD-FR/CSCG/csrd-20241001.json" )
 
     return queryInterface.sequelize.transaction(t => {
       return Promise.all([
-        queryInterface.bulkInsert('AbilityCategory', ab_categ_list, { transaction: t }),
+        queryInterface.bulkInsert('AbilityCategories', ab_categ_list, { transaction: t }),
       ]);
     
     });
